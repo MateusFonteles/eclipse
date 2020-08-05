437f24523efba9c60c09c4aa18473c746a16f386
MateusFonteles <51653081+MateusFonteles@users.noreply.github.com>
2020-08-05 08:14:13 -0300

    adicionei arquivos via upload

---
+++ interacaoGit/src/mateusFonteles/helloGit.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package mateusFonteles;
+
+/**
+ * @author mrmat
+ *
+ */
+public class helloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("hello git");
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ interacaoGit/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author mrmat
+ *
+ */
+module interacaoGit {
+}
